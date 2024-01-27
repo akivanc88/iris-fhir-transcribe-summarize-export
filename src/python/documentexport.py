04ab30eae7db4f09e38b2f09592a7cbda08679af
akivanc88 <er.akashmajumder@gmail.com>
2024-01-26 16:13:57 -0800

    Update documentexport.py
    updated to GPT-4 preview

--- src/python/documentexport.py
+++ src/python/documentexport.py
@@ -12,7 +12,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def Summarize(text):
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model="gpt-4-turbo-preview",
         prompt="Summarize the following text and give title and summary in json format. \
                 Sample output - {\"title\": \"some-title\", \"summary\": \"some-summary\"}.\
                 Input - "
